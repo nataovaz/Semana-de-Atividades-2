bebe7cbfead0c6f1d151aeac9a79a410c7363da0
Natan Vaz <natanvaz27@unifei.edu.br>
2021-09-16 17:11:57 -0300

    Add files via upload

---
+++ SemanaAtv2/1.py
@@ -0,0 +1,31 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if a > b and b > c:
+    print(c)
+    print(b)
+    print(a)
+elif a > c and c > b :
+    print(b)
+    print(c)
+    print(a)
+elif b > a and a > c:
+    print(c)
+    print(a)
+    print(b)
+elif b > c and c > a:
+    print(a)
+    print(c)
+    print(b)
+elif c > a and a > b:
+    print(b)
+    print(a)
+    print(c)
+elif c > a and b > a:
+    print(a)
+    print(b)
+    print(c)
+print()
+print (a)
+print (b)
+print (c)
---
+++ SemanaAtv2/3.py
@@ -0,0 +1,27 @@
+x=int(input())
+if(x==1):
+    print("Janeiro")
+elif(x==2):
+    print("Fevereiro")
+elif(x==3):
+    print("Março")
+elif(x==4):
+    print("Abril")
+elif(x==5):
+    print("Maio")
+elif(x==6):
+    print("Junho")
+elif(x==7):
+    print("Julho")
+elif(x==8):
+    print("Agosto")
+elif(x==9):
+    print("Setembro")
+elif(x==10):
+    print("Outubro")
+elif(x==11):
+    print("Novembro")
+elif(x==12):
+    print("Dezembro")
+else:
+    print("Esta entrada não corresponde o desafio...")
